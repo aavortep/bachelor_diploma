16827fb64e8b589414356e05dac8c301e56cf378
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-16 16:52:54 +0300

    add midi splitting (divides on tracks)

--- src/split_audio.py
+++ src/split_audio.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
 import os
+import mido
 
 
 def split_mp3(audio_path: str):
@@ -16,5 +17,49 @@ def split_mp3(audio_path: str):
         part.export(f"tmp/part_{i}.mp3", format="mp3")
 
 
+def split_midi(audio_path: str):
+    if not os.path.isdir("tmp"):
+        os.mkdir("tmp")
+
+    # загрузка MIDI файла
+    midi_file = mido.MidiFile(audio_path)
+
+    # определение длительности сегментов в миллисекундах
+    segment_length = 2000
+
+    # создание списка сегментов
+    segments = []
+    current_segment = []
+    current_tick = 0
+
+    # обход всех сообщений в треках
+    for track in midi_file.tracks:
+        for msg in track:
+            # проверка, достигнут ли конец текущего сегмента
+            if msg.time + current_tick >= segment_length:
+                # добавление текущего сегмента в список и создание нового
+                segments.append(current_segment)
+                current_segment = [msg]
+                current_tick = msg.time
+            else:
+                # добавление сообщения в текущий сегмент и увеличение текущего времени
+                current_segment.append(msg)
+                current_tick += msg.time
+
+    # добавление последнего сегмента в список
+    segments.append(current_segment)
+
+    # сохранение каждого сегмента в отдельный файл
+    for i, segment in enumerate(segments):
+        # создание нового MIDI файла и добавление трека с текущим сегментом
+        new_midi = mido.MidiFile()
+        new_track = mido.MidiTrack()
+        new_midi.tracks.append(new_track)
+        for msg in segment:
+            new_track.append(msg)
+        # сохранение файла
+        new_midi.save(f'tmp/segment_{i}.mid')
+
+
 if __name__ == "__main__":
-    split_mp3("test_audio/chop.mp3")
+    split_midi("test_audio/chop.mid")
