74b14b2e9b9fe786581d945f08deb575a39b3426
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-16 19:45:51 +0300

    visualize tempo and rhythm distributions

--- src/training.py
+++ src/training.py
@@ -1,8 +1,32 @@
 import pymc3 as pm
 import numpy as np
 import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
 
-spotify_data = pd.read_csv('tempo_dataset.csv')
-bpm_data = spotify_data['tempo']
-rhythm_data = spotify_data['time_signature']
-print(bpm_data, rhythm_data)
+
+def show_distributions(bpm_data, rhythm_data):
+    bpm_list = list(map(round, bpm_data.array))
+    bpm_dict = dict(Counter(bpm_list))
+    bpms = bpm_dict.keys()
+    bpm_counts = bpm_dict.values()
+    plt.bar(bpms, bpm_counts)
+    plt.xlabel("BPM")
+    plt.ylabel("Кол-во")
+    plt.show()
+
+    rhythm_list = list(rhythm_data.array)
+    rhythm_dict = dict(Counter(rhythm_list))
+    rhythms = rhythm_dict.keys()
+    rhythm_counts = rhythm_dict.values()
+    plt.bar(rhythms, rhythm_counts)
+    plt.xlabel("Размеры")
+    plt.ylabel("Кол-во")
+    plt.show()
+
+
+if __name__ == "__main__":
+    spotify_data = pd.read_csv('tempo_dataset.csv')
+    bpm_data = spotify_data['tempo']
+    rhythm_data = spotify_data['time_signature']
+    show_distributions(bpm_data, rhythm_data)
