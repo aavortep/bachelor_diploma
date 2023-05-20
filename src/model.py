24f308a7c3eba74489397cb11a57cc4d18aa0681
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-20 20:10:27 +0300

    add measure estimation

--- src/model.py
+++ src/model.py
@@ -5,7 +5,38 @@ import librosa
 from scipy import stats
 
 
-def estimate_bpm(audio_path: str, bpm_dataset):
+def estimate_rhythm(audio_path: str) -> int:
+    y, sr = librosa.load(audio_path)
+
+    # сила "нажатия" на ноты
+    onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+    # пиковые силы "нажатия" на ноты
+    peaks = librosa.util.peak_pick(onset_env, pre_max=3, post_max=3, pre_avg=3,
+                                   post_avg=5, delta=0.5, wait=10)
+    peaks_time = librosa.frames_to_time(peaks, sr=sr)
+
+    _, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
+    # beat_strengths = onset_env[beats]
+    beats_time = librosa.frames_to_time(beats, sr=sr)
+    # начала тактов
+    downbeats = {}
+    for i, beat in enumerate(beats_time):
+        if beat in peaks_time:
+            downbeats[i] = beat
+    # print(downbeats)
+
+    downbeats_nums = sorted(list(downbeats.keys()))
+    difs_sum = 0
+    for i in range(1, len(downbeats_nums)):
+        difs_sum += downbeats_nums[i] - downbeats_nums[i - 1]
+
+    # среднее число ударов в такте (средний размер)
+    avg_measure = difs_sum / (len(downbeats_nums) - 1)
+
+    return round(avg_measure)+1
+
+
+def estimate_bpm(audio_path: str, bpm_dataset) -> float:
     y, sr = librosa.load(audio_path)
     spectr = librosa.feature.melspectrogram(y=y, sr=sr)
     autocorr = librosa.autocorrelate(spectr)
@@ -37,11 +68,13 @@ def estimate_bpm(audio_path: str, bpm_dataset):
     # наибольшая плотность - наиболее вероятный темп
     bpm_estimate = tempos[density(tempos).argmax()]
 
-    return bpm_estimate
+    return float(bpm_estimate)
 
 
 if __name__ == "__main__":
     # загрузка данных
     spotify_data = pd.read_csv('tempo_dataset.csv')
+    music = 'test_audio/vo_sne_guitars.mp3'
 
-    print('Estimated tempo: ', estimate_bpm('test_audio/master.mp3', spotify_data['tempo']))
+    print('Estimated tempo: ', estimate_bpm(music, spotify_data['tempo']))
+    print('Estimated time signature: ', estimate_rhythm(music))
